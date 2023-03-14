deee4d5d2f4cbec0730be911cc30483daf4a64d8
Pasha <g1ebikpasha@gmail.com>
2023-03-14 19:33:58 +0800

    AppLink
    Signed-off-by: Pasha <g1ebikpasha@gmail.com>

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type ModsType = Record<string, string | boolean>
 
-export const classNames = (cls: string, mods: ModsType, additional: string[]): string => (
+export const classNames = (cls: string, mods: ModsType = {}, additional: string[] = []): string => (
     [
         cls,
-        ...additional,
+        ...additional.filter(Boolean),
         Object.entries(mods)
             .filter(([_, value]) => Boolean(value))
             .map(([className]) => className)
