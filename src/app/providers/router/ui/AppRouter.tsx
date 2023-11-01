9a3c5449b809e81c104619d61affc333f254e236
Pasha <g1ebikpasha@gmail.com>
2023-11-01 17:55:28 +0300

    NotFound page, Loader, PageLoader
    Signed-off-by: Pasha <g1ebikpasha@gmail.com>

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,21 +1,22 @@
-import React, { Suspense } from 'react'
+import { Suspense } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/routeConfig'
+import { PageLoader } from 'widgets/PageLoader'
 
 export const AppRouter = () => {
     const { t } = useTranslation()
     return (
-        <Suspense fallback={<h1>{t('Загрузка...')}</h1>}>
-            <Routes>
-                {Object.values(routeConfig).map(({ path, element }) => (
-                    <Route key={path} path={path} element={
-                        <div className='page-wrapper'>
+        <Routes>
+            {Object.values(routeConfig).map(({ path, element }) => (
+                <Route key={path} path={path} element={
+                    <div className='page-wrapper'>
+                        <Suspense fallback={<h1><PageLoader /></h1>}>
                             {element}
-                        </div>
-                    } />
-                ))}
-            </Routes>
-        </Suspense>
+                        </Suspense>
+                    </div>
+                } />
+            ))}
+        </Routes>
     )
 }
--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,15 +1,18 @@
 import { About } from 'pages/About'
 import { Main } from 'pages/Main'
+import { NotFound } from 'pages/NotFound'
 import { type RouteProps } from 'react-router-dom'
 
 export enum AppRoutes {
     MAIN = 'main',
-    ABOUT = 'about'
+    ABOUT = 'about',
+    NOT_FOUND = 'notFound'
 }
 
 export const RoutePaths: Record<AppRoutes, string> = {
     [AppRoutes.MAIN]: '/',
-    [AppRoutes.ABOUT]: '/about'
+    [AppRoutes.ABOUT]: '/about',
+    [AppRoutes.NOT_FOUND]: '*'
 }
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
@@ -20,5 +23,9 @@ export const routeConfig: Record<AppRoutes, RouteProps> = {
     [AppRoutes.ABOUT]: {
         path: RoutePaths.about,
         element: <About />
+    },
+    [AppRoutes.NOT_FOUND]: {
+        path: RoutePaths.notFound,
+        element: <NotFound />
     }
 }
